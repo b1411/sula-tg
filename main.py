7c20f892cc73d05d3052adabb958dfa8a6b4802d
Rakhmat <kenzhebaev05@gmail.com>
2024-03-18 18:16:32 +0500

    sultan

---
+++ main.py
@@ -0,0 +1,45 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, types, Router
+from aiogram.filters import CommandStart, Command
+from aiogram.enums import ParseMode
+import requests
+from aiogram.methods.close import Close
+
+dp = Dispatcher()
+
+TOKEN = getenv('TOKEN')
+
+
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    await message.answer("Здравствуйте! Я бот-помощник Султан. Чем могу помочь?")
+
+
+@dp.message(Command('end'))
+async def end(message: types.Message):
+    requests.post('http://jasik.alwaysdata.net/clear-ig-session', json={"contactId": message.from_user.id})
+    await message.bot.close()
+
+
+@dp.message()
+async def message_handler(message: types.Message):
+    req = {
+        "message": message.text,
+        "contactId": message.from_user.id
+    }
+    res = requests.post('http://jasik.alwaysdata.net/qazai', json=req)
+    await message.answer(res.json()['message'])
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
